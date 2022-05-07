6f20adad7fe620c0196165180b184b40b527e694
kiqueangulo <kiqueangulo@gmail.com>
2022-05-07 19:28:36 -0400

    Specific restaurant page added

--- controllers/places.js
+++ controllers/places.js
@@ -1,28 +1,33 @@
 const router = require('express').Router();
 
+let places = [
+    {
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, 
+    {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }
+];
+
 router.get('/', (req, res) => {
-    let places = [
-        {
-            name: 'H-Thai-ML',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Thai, Pan-Asian',
-            pic: 'http://placekitten.com/250/250'
-        }, 
-        {
-            name: 'Coding Cat Cafe',
-            city: 'Phoenix',
-            state: 'AZ',
-            cuisines: 'Coffee, Bakery',
-            pic: 'http://placekitten.com/250/250'
-        }
-    ];
-      
-    res.render('places/index', {places})
+    res.render('places/index', { places })
 });
 
 router.get('/new', (req, res) => {
     res.render('places/new')
+});
+
+router.get('/:id', (req, res) => {
+    let page = places[req.params.id];
+    res.render('places/id', { page })
 });
 
 module.exports = router;
---
+++ views/places/id.jsx
@@ -0,0 +1,18 @@
+const React = require('react');
+const Def = require('../default.jsx');
+
+function idInfo(data) {
+    let info = data.page;
+    
+    return (
+        <Def>
+            <main>
+                <h1>Specific Place</h1>
+                <h2>{info.name}</h2>
+                <img src={info.pic} alt={info.name} />
+            </main>
+        </Def>
+    )
+};
+
+module.exports = idInfo;
