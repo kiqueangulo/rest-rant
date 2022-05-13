38a0394e4d8dc497fea411e50e0c1b6ce7f54929
kiqueangulo <kiqueangulo@gmail.com>
2022-05-12 22:32:15 -0400

    Crearing form for new place

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -5,11 +5,29 @@ function newPage() {
     return (
         <Def>
             <main>
-                <form action="">
-                    <label htmlFor="">New Rest Page</label>
-                    <br />
-                    <input type="text" />
-                    <button>PUBLIC</button>
+                <h1>Add a New Place</h1>
+                <form action="/places" method='POST'>
+                    <div>
+                        <label htmlFor="name">Place Name</label>
+                        <input type="text" id='name' name='name' required />
+                    </div>
+                    <div>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input type="url" id='pic' name='pic' />
+                    </div>
+                    <div>
+                        <label htmlFor="city">City</label>
+                        <input type="text" id='city' name='city' />
+                    </div>
+                    <div>
+                        <label htmlFor="state">State</label>
+                        <input type="text" id='state' name='state' />
+                    </div>
+                    <div>
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input type="text" id='cuisines' name='cuisines' required />
+                    </div>
+                    <input type="submit" value="Add Place" />
                 </form>
             </main>
         </Def>
