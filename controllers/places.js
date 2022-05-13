6643fb0aa48eb599e03f7113f7cc526a573ed625
kiqueangulo <kiqueangulo@gmail.com>
2022-05-12 23:01:17 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -10,8 +10,19 @@ router.get('/new', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('POST /places')
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    };
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    };
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    };
+
+    places.push(req.body);
+    res.redirect('/places')
 });
 
 router.get('/:id', (req, res) => {
