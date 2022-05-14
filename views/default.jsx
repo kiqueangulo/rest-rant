8ca799baca509cd0bbd67e55c50cffd0b097b791
kiqueangulo <kiqueangulo@gmail.com>
2022-05-14 16:48:11 -0400

    Nav bar styled

--- views/default.jsx
+++ views/default.jsx
@@ -9,20 +9,29 @@ function Def(html) {
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="/">Home</a>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <p class="navbar-brand">Rest-Rant</p>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Home</a>
                         </li>
-                        <li>
-                            <a href="/places">Places</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/places">Places</a>
                         </li>
-                        <li>
-                            <a href="/places/new">Add Place</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/places/new">Add Place</a>
                         </li>
-                    </ul>
+                        </ul>
+                    </div>
                 </nav>
                 {html.children}
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
             </body>
         </html>
     )
