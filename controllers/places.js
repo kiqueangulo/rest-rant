614930681a6566d861836b94ea38441187c37ea6
kiqueangulo <kiqueangulo@gmail.com>
2022-05-14 16:27:30 -0400

    Delete button correctly redirects and updates data

--- controllers/places.js
+++ controllers/places.js
@@ -46,4 +46,18 @@ router.get('/:id/edit', (req, res) => {
     res.render('places/editPlace', { page })
 });
 
+// Delete
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+        res.render('error404')
+    } else {
+        places.splice(id, 1);
+        res.redirect('/places')
+    };
+})
+
 module.exports = router;
