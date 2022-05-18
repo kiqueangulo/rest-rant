8722864f054f2f24cd2daf8e1f94c5ba182e30a1
kiqueangulo <kiqueangulo@gmail.com>
2022-05-18 14:40:00 -0400

    Setting up imdex page with Mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,25 +1,18 @@
 const router = require('express').Router();
-const places = require('../models/places.js');
+const db = require('../models');
 
 // Index (show restaurants)
 router.get('/', (req, res) => {
-    res.render('places/index', { places })
+    db.PLace.find()
+        .then( places => res.render('places/index', { places }))
+        .catch( err => {
+            console.log(err);
+            res.render('error404')
+        })
 });
 
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
-        // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
-    };
-    if (!req.body.city) {
-        req.body.city = 'Anytown'
-    };
-    if (!req.body.state) {
-        req.body.state = 'USA'
-    };
-    
-    places.push(req.body);
-    res.redirect('/places')
+    res.send('POST /places stub')
 });
 
 // New rest
@@ -29,67 +22,22 @@ router.get('/new', (req, res) => {
 
 // Show specific
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id);
-
-    if (isNaN(id)) {
-        res.render('error404')
-    } else if (!places[id]) {
-        res.render('error404')
-    } else {
-        res.render('places/id', { place: places[id], id })
-    }
+    res.send('GET /places/:id stub')
 });
 
 // Redirect from Edit to Show (id)
 router.put('/:id', (req, res) => {
-    let id = Number(req.params.id);
-
-    if (isNaN(id)) {
-        res.render('error404')
-    } else if (!places[id]) {
-        res.render('error404')
-    } else {
-        // Making sure req.body data is valid
-        if (!req.body.pic) {
-            req.body.pic = 'http://placekitten.com/400/400'
-        };
-        if (!req.body.city) {
-            req.body.city = 'Anytown'
-        };
-        if (!req.body.state) {
-            req.body.state = 'USA'
-        };
-
-        places[id] = req.body;  // Save the new data into places[id] (update)
-        res.redirect(`/places/${id}`)
-    };
+    res.send('PUT /places/:id stub')
 });
 
 // Delete
 router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id);
-
-    if (isNaN(id)) {
-        res.render('error404')
-    } else if (!places[id]) {
-        res.render('error404')
-    } else {
-        places.splice(id, 1);
-        res.redirect('/places')
-    };
+    res.send('DELETE /places/:id stub')
 });
 
 // Edit
 router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id);
-
-    if (isNaN(id)) {
-        res.render('error404')
-    } else if (!places[id]) {
-        res.render('error404')
-    } else {
-        res.render('places/editPlace', { place: places[id], id })
-    };
+    res.send('GET edit form stub')
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
-});
+}, () => (
+  console.log(`Connected to mongo: ${process.env.MONGO_URI}`)
+));
 
 module.exports.PLace = require('./places');
