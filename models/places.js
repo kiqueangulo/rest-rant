14985e411b3d03289dd45e7910c46b46b734eccb
kiqueangulo <kiqueangulo@gmail.com>
2022-05-18 11:13:39 -0400

    Setting up Mongoose

--- models/places.js
+++ models/places.js
@@ -1,4 +1,19 @@
-module.exports = [
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema);
+
+
+
+/* module.exports = [
     {
         name: 'H-Thai-ML',
         city: 'Seattle',
@@ -13,4 +28,4 @@ module.exports = [
         cuisines: 'Coffee, Bakery',
         pic: '/images/olive-garden.webp'
     }
-];
+]; */
